b3ef487cc75827dc455115c675246ed26074bfb9
Bhanu Khapra <119013859+Khapra@users.noreply.github.com>
2023-03-30 02:49:18 -0700

    Update rolememberlimit.py
    1. Added persistence.
    2. Grouped the commands under rolelimit.
    3. Added remove limit command.
    4. Added list limit command.
    6. Made changes so the messages are more informative.
    5. Better error handling.

--- RoleMemberLimit/rolememberlimit.py
+++ RoleMemberLimit/rolememberlimit.py
@@ -1,5 +1,6 @@
 import discord
-from redbot.core import commands
+from redbot.core import commands, Config
+
 
 class RoleMemberLimit(commands.Cog):
     """
@@ -7,27 +8,60 @@ class RoleMemberLimit(commands.Cog):
     """
 
     __author__ = ["Khapra"]
-    __version__ = "0.0.1"
+    __version__ = "0.0.4"
 
     def __init__(self, bot):
         self.bot = bot
-        self.role_limits = {}
+        self.config = Config.get_conf(self, identifier=534216890234)
+        self.config.register_guild(role_limits={})
 
-        
+    @commands.group()
+    async def rolelimit(self, ctx):
+        """Group command for setting and retrieving role limits."""
+        pass
+
+    @rolelimit.command(name="set")
     @commands.has_permissions(administrator=True)
-    @commands.command()
-    async def setrolelimit(self, ctx, role: discord.Role, limit: int):
+    async def set_role_limit(self, ctx, role: discord.Role, limit: int):
         """Sets the maximum number of members allowed in the specified role."""
-        self.role_limits[role.id] = limit
+        async with self.config.guild(ctx.guild).role_limits() as role_limits:
+            role_limits[str(role.id)] = limit
         await ctx.send(f"Maximum limit for {role.name} set to {limit} members.")
-        
+
+    @rolelimit.command(name="list")
+    async def list_role_limits(self, ctx):
+        """Retrieves the maximum member limits set for each role in the server."""
+        role_limits = await self.config.guild(ctx.guild).role_limits()
+        if not role_limits:
+            await ctx.send("No limits set.")
+        else:
+            msg = "Role limits set in this server:\n"
+            for role_id, limit in role_limits.items():
+                role = ctx.guild.get_role(int(role_id))
+                if role:
+                    msg += f"{role.name}: {limit}\n"
+            await ctx.send(msg)
+
+    @rolelimit.command(name="remove")
+    @commands.has_permissions(administrator=True)
+    async def remove_role_limit(self, ctx, role: discord.Role):
+        """Removes the maximum number of members allowed in the specified role."""
+        async with self.config.guild(ctx.guild).role_limits() as role_limits:
+            if str(role.id) in role_limits:
+                del role_limits[str(role.id)]
+                await ctx.send(f"Maximum limit for {role.name} removed.")
+            else:
+                await ctx.send("No limit set for this role.")
+
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         """Checks if a member has reached the maximum limit for a role."""
+        role_limits = await self.config.guild(after.guild).role_limits()
         for role in after.roles:
-            if role.id in self.role_limits and len(role.members) > self.role_limits[role.id]:
+            if str(role.id) in role_limits and len(role.members) > role_limits[str(role.id)]:
                 await after.remove_roles(role)
-                await after.send(f"You have been removed from the {role.name} role because the maximum limit of {self.role_limits[role.id]} members has been reached.")
+                await after.send(f"You have been removed from the {role.name} role because the maximum limit of {role_limits[str(role.id)]} members has been reached.")
+
 
 def setup(bot):
     bot.add_cog(RoleMemberLimit(bot))
